f31a99bf5ec7ce398b0949cc83de4ee460439393
atopalos <atopaloudis@gmail.com>
2013-07-11 00:26:48 +0000

    grader.js changed to take a URL as an input but... unsuccessfully so far

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLURL_DEFAULT = "http://pacific-atoll-5168.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +57,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+//var checkHtmlUrl = function(htmlUrl, checksfile) {
+//    $ = htmlUrl;
+//    var checks = loadChecks(checksfile).sort();
+//    var out = {};
+//    for(var ii in checks) {
+//	var present = $(checks[ii]).length > 0;
+//	out[checks[ii]] = present;
+//    }
+//    return out;
+//};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,8 +78,10 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_url>', 'Url to index.html')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
+//    var checkJson = checkHtmlUrl(clone(rest.get(program.url)), program.checks); 
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
